f5e8944b87a488a1478e015026fa551760df7b0c
ElJakes <jidejacobs1@gmail.com>
2023-09-16 23:15:30 +0100

    update html css and txt file, render crypto name and image

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const imgAuthor = document.getElementById('img-author')
+const cryptoData = document.getElementById("crypto-data");
+const imgAuthor = document.getElementById("img-author");
 
 fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=fnkdnkskmadndcknk"
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((response) => response.json())
   .then((data) => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
-    imgAuthor.textContent = `Author: ${data.user.name}`
-})
-.catch(err => {
-      document.body.style.backgroundImage = `url(https://cdn.xxl.thumbs.canstockphoto.com/danger-sign-drawing_csp0665020.jpg)`;
-
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    imgAuthor.textContent = `Author: ${data.user.name}`;
   })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1500382017468-9049fed747ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTQ4OTg2MTZ8&ixlib=rb-4.0.3&q=80&w=1080)`;
+  });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((response) => response.json())
+  .then((data) => {
+    cryptoData.innerHTML = `<span>${data.name}</span> <img src='${data.image.small}' />`
+  })
+  .catch((err) => console.log("error found"));

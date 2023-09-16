cf48449453ff4717c64491a711c37498b43cc0fe
ElJakes <jidejacobs1@gmail.com>
2023-09-16 20:35:35 +0100

    update txt file, update html, fetch image dats from Unsplash API

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert('working')
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+  })

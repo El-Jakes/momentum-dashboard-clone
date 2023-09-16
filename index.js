fa7ab5cfd3c1e22ac32351d6d082332075a2fcae
ElJakes <jidejacobs1@gmail.com>
2023-09-16 21:50:56 +0100

    update txt file, testrun .catch() method in JS file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const imgAuthor = document.getElementById('img-author')
 
 fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=fnkdnkskmadndcknk"
 )
   .then((response) => response.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     imgAuthor.textContent = `Author: ${data.user.name}`
+})
+.catch(err => {
+      document.body.style.backgroundImage = `url(https://cdn.xxl.thumbs.canstockphoto.com/danger-sign-drawing_csp0665020.jpg)`;
+
   })

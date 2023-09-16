f2ce70b8cfab4c81c750fb13509e3622b9016a93
ElJakes <jidejacobs1@gmail.com>
2023-09-16 20:52:09 +0100

    render image author name on the screen, update html,css and txt files

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const imgAuthor = document.getElementById('img-author')
+
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((response) => response.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`
+    imgAuthor.textContent = `Author: ${data.user.name}`
   })
